7ea78eaf9c5bd76f5e22d2c3517576c2f0efadd2
twentysixhugs <iaan.lamanosau@protonmail.com>
2023-04-29 17:15:20 +0300

    article reading, responsive design, seo, category selection

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,21 @@
-import { AboutView } from "@views/about/view";
 import { NextPage } from "next";
 import Head from "next/head";
+
+import { AboutView } from "@views/about/view";
 
 const About: NextPage = () => {
   return (
     <>
       <Head>
         <title>About me |  </title>
+        <meta name="description" content="The story behind why I write" />
+        <meta property="og:title" content="About me -  " />
+        <meta
+          property="og:description"
+          content="The story behind why I write"
+        />
+        <meta property="og:url" content="https://.dev/about" />
+        <meta property="og:type" content="website" />
       </Head>
       <AboutView />
     </>
